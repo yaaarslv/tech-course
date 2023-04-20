1ce2ea5b163305dd84c4cabe91160fb0d81df406
yaarslv <savenkovyaroslav53@gmail.com>
2023-04-20 22:02:07 +0300

    lab-3: Done

---
+++ lab-3/src/main/java/applications/Application.java
@@ -0,0 +1,15 @@
+package applications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = "services")
+@EntityScan(basePackages = "entities")
+@EnableJpaRepositories(basePackages = "repositories")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
